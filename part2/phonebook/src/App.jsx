fdaa8b88a92fbf0e397bf7c3470dce998789db2f
Maxxy21 <nanabaah12@gmail.com>
2024-11-06 16:50:19 +0100

    exercises 2.6.2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,30 +1,27 @@
 import { useState } from "react";
 
-const Filter = () => {
+const Filter = ({ filter, handleFilterChange }) => {
   return (
     <div>
-      filter shown with <input />
+      filter shown with <input value={filter} onChange={handleFilterChange} />
     </div>
   );
 };
 
-const PersonForm = ({ persons, setPersons, newName, setNewName }) => {
-  const addPerson = (event) => {
-    event.preventDefault();
-    const personObject = {
-      name: newName,
-      number: Math.floor(Math.random() * 1000000000),
-      id: persons.length + 1,
-    };
-
-    setPersons(persons.concat(personObject));
-    setNewName("");
-  };
-
+const PersonForm = ({
+  addPerson,
+  newName,
+  handlePersonChange,
+  newNumber,
+  handleNumberChange,
+}) => {
   return (
     <form onSubmit={addPerson}>
       <div>
-        name: <input />
+        name: <input value={newName} onChange={handlePersonChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
       </div>
       <div>
         <button type="submit">add</button>
@@ -33,46 +30,101 @@ const PersonForm = ({ persons, setPersons, newName, setNewName }) => {
   );
 };
 
-const Persons = () => {
+const AllPersons = ({ persons }) => {
   return (
     <div>
-      <ul>
-        <li>Arto Hellas</li>
-        <li>Ada Lovelace</li>
-        <li>Dan Abramov</li>
-        <li>Mary Poppendieck</li>
-      </ul>
+      {persons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const FilteredPersons = ({ persons, filter }) => {
+  return (
+    <div>
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((person) => (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        ))}
+    </div>
+  );
+};
+
+const Persons = ({ persons, filter }) => {
+  return (
+    <div>
+      {filter === "" ? (
+        <AllPersons persons={persons} />
+      ) : (
+        <FilteredPersons persons={persons} filter={filter} />
+      )}
     </div>
   );
 };
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+    { name: "Arto Hellas", number: "040-123456" },
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
 
-      <Filter />
-
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>Add a new</h3>
-
       <PersonForm
-        persons={persons}
-        setPersons={setPersons}
+        addPerson={addPerson}
         newName={newName}
-        setNewName={setNewName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
       />
+      <h2>Numbers</h2>
 
-      <h3>Numbers</h3>
-
-      <Persons />
+      <Persons persons={persons} filter={filter} />
     </div>
   );
 };

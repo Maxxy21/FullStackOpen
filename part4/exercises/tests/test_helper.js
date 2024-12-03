b45730e7c5f83eaf5948717c24c612a9a1ea36a7
Maxxy21 <nanabaah12@gmail.com>
2024-12-03 10:58:30 +0100

    part5

---
+++ part4/exercises/tests/test_helper.js
@@ -0,0 +1,12 @@
+const User = require('../models/user')
+
+
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(user => user.toJSON())
+}
+
+
+module.exports = {
+    usersInDb
+}
